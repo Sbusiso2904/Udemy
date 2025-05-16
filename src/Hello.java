5899cb0e07f69dcefe760ada98dc38351343efb2
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-05-16 09:51:05 +0200

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,42 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Akha!");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topscore = 80;
+        if (topscore < 100){
+            System.out.println("You got the high score");
+        }
+
+        int secondTopscore = 95 ;
+        if ((topscore > secondTopscore) && ( topscore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topscore > 90) || (secondTopscore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+         if(newValue == 50){
+             System.out.println("This is true");
+         }
+
+         boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        double var = 20.00;
+        double vaz = 80.00;
+        double var + double vaz * 100.00;
+    }
+}
