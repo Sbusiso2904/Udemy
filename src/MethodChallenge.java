3561fa246922ed3f728a0d08d77727e518515bff
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-05-29 19:07:16 +0200

    48=49

--- src/MethodChallenge.java
+++ src/MethodChallenge.java
@@ -1,10 +1,10 @@
 public class MethodChallenge {
     public static void main(String[] args) {
+
     }
 
     public static void diplayHighScorePosition( String playerName, int player_position){
         System.out.println (playerName + " Tim managed to get into position " + player_position + " on the high score list");
-
 
     }
 }
