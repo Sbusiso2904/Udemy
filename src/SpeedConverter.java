ef0f636b0dd3100fb0b869f61e86ffcff080b274
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-04 15:57:49 +0200

    kk

---
+++ src/SpeedConverter.java
@@ -0,0 +1,24 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometerPerHour){
+        printConversion(kilometerPerHour);
+
+
+        if(kilometerPerHour < 0){
+            return -1;
+        }
+       return Math.round(kilometerPerHour / 1.609);
+
+    }
+
+    public static void  printConversion(double kilometerPerHour){
+
+        if (kilometerPerHour < 0){
+            System.out.println("Invalid value");
+            return;
+        }
+        long milesPerHour = toMilesPerHour(kilometerPerHour);
+        System.out.println(kilometerPerHour + "km/h" + milesPerHour + "mi/h");
+
+    }
+}
