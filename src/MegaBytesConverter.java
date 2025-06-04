1bea887a38308b0bd801278632b1264af8e04b9c
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-04 22:02:57 +0200

    just push

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,23 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+//        YY = megabytes;
+//        XX = kiloBytes;
+//        ZZ =  kiloBytes;
+        Math.(kiloBytes / 2500);
+
+
+        if(kiloBytes < 0){
+            System.out.println("Invalid value")
+        }
+    }
+
+    public static int printMegaBytesAndKiloBytes(){
+        if(kiloBytes == 2500){
+            System.static.println("2500 KB = 2 MB and 452 KB");
+        }else(kiloBytes < 0){
+            System.out.println("Invalid value");
+        }else if(kiloBytes == 5000){
+            System.out.println("KB = 4 MB and 904 KB");
+        }
+    }
+}
