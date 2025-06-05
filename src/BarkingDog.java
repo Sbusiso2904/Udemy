39ceb43a0b93cc46a2ee1f52b396ff4f25c1dedb
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-05 15:25:21 +0200

    pushing p1

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,30 +1,43 @@
 public class BarkingDog {
-    public static boolean shouldWakeUp(boolean Sam, int hourOfDay){
-        //int hrs = hourOfDay;
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        //     //int hrs = hourOfDay;
 
-        if(hourOfDay, int 0 >= 23){
+        //     if(hourOfDay, int 0 >= 23){
+        //         return false;
+
+        //     }else(hourOfDay == valid){
+        //         System.out.println(Sam + "should wake up");
+
+        //     }else if(Sam > 23){
+        //         return true;
+        //     }
+
+
+        //     // if(Sam <= int 23 hrs);
+        //     //     Math.Sam(hourOfDay > 23);
+
+        //     return false;
+        //         System.out.println(Sam + "He's not barking");
+
+        //      if(Sam < 0 || Sam > 23);
+        //      return false;
+        //         System.out.println("Invalid input");
+
+        // }
+        // return true;
+
+        // if the hourOfDay is less than 0 or greater than 23, it's not
+        // valid, so return false
+
+        if (hourOfDay < 0 || hourOfDay > 23){
             return false;
-
-        }else(hourOfDay == valid){
-            System.out.println(Sam + "should wake up");
-
-        }else if(Sam > 23){
-            return true;
         }
+        // if the dog is barking, and it's before 8am or after 10pm,
+        // then you should wake up.
 
-
-        // if(Sam <= int 23 hrs);
-        //     Math.Sam(hourOfDay > 23);
-
-        return false;
-        System.out.println(Sam + "He's not barking");
-
-        if(Sam < 0 || Sam > 23);
-        return false;
-        System.out.println("Invalid input");
+        return barking && (hourOfDay < 8 || hourOfDay > 22);
 
     }
-    return true;
 }
 
 
