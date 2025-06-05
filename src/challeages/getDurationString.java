08a88b0573cc045f59310f8e7ea43a0f29d11595
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-05 18:22:01 +0200

    pushing p

--- src/challeages/getDurationString.java
+++ src/challeages/getDurationString.java
@@ -1,4 +1,10 @@
 package challeages;
 
-public class getDurationString {
+public class getDurationString(int seconds) {
+
+    //Two step approach to get hours
+    int minutes = seconds / 60;
+    int hours = minutes / 60;
+
+    //One step approach to get hours
 }
