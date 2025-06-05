42f1eecde87babcb9c381ff86864b55182d71bb8
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-05 15:14:12 +0200

    pushing p

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,14 +1,27 @@
 public class BarkingDog {
-    public static boolean shoulWakeUp(boolean Sam, int hourOfDay){
-        int hrs = hourOfDay;
-        if(Sam <= int 23 hrs);
-        Math.Sam(hrs > 23);
+    public static boolean shouldWakeUp(boolean Sam, int hourOfDay){
+        //int hrs = hourOfDay;
+
+        if(hourOfDay, int 0 >= 23){
+            return false;
+
+        }else(hourOfDay == valid){
+            System.out.println(Sam + "should wake up");
+
+        }else if(Sam > 23){
+            return true;
+        }
+
+
+        // if(Sam <= int 23 hrs);
+        //     Math.Sam(hourOfDay > 23);
 
         return false;
-            System.out.println(Sam + "He's not barking");
+        System.out.println(Sam + "He's not barking");
 
         if(Sam < 0 || Sam > 23);
         return false;
+        System.out.println("Invalid input");
 
     }
     return true;
