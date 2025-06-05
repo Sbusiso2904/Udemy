fc013384738f0998a7d20d7ebf504db9ba30c0d7
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-05 23:49:10 +0200

    pushing p

--- src/challeages/getDurationString.java
+++ src/challeages/getDurationString.java
@@ -1,5 +1,8 @@
 package challeages;
 
+public static void main(String[] args){
+    System.out.println(getDurationString(3945));
+}
 public class getDurationString(int seconds) {
 
     //Two step approach to get hours
@@ -7,9 +10,19 @@ public class getDurationString(int seconds) {
     int hours = minutes / 60;
     System.out.println("hours =" + hours);
 
+    int remainingMinutes = minutes % 60;
+    System.out.println("minutes =" + minutes);
+    System.out.println("remainingMinutes =" + remainingMinutes);
+
     //One step approach to get hours
-    int hours1 = minutes + seconds / 3600;
-    System.out.println("hours1 = " + hours1)
+//    int hours1 = minutes + seconds / 3600;
+//    System.out.println("hours1 = " + hours1);
+
+    return"";
+
+    public static string getDurationString(int minutes, int seconds){
+        return "";
+    }
 
 
     System.out.println("m")
