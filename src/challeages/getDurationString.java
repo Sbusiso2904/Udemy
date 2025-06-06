658f2e9afa6bfd94efcf88cdcb9731cf4cb85255
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-06 13:09:46 +0200

    pushing p

--- src/challeages/getDurationString.java
+++ src/challeages/getDurationString.java
@@ -1,32 +1,42 @@
 package challeages;
 
-public static void main(String[] args){
-    System.out.println(getDurationString(3945));
-}
-public class getDurationString(int seconds) {
+public static void main  (String[] args){
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(65, 45));
 
-    //Two step approach to get hours
-    int minutes = seconds / 60;
-    int hours = minutes / 60;
-    System.out.println("hours =" + hours);
 
-    int remainingMinutes = minutes % 60;
-    System.out.println("minutes =" + minutes);
-    System.out.println("remainingMinutes =" + remainingMinutes);
+    public class String getDurationString(int seconds) {
 
-    //One step approach to get hours
-//    int hours1 = minutes + seconds / 3600;
-//    System.out.println("hours1 = " + hours1);
+        int minutes = seconds / 60;
+        return getDurationString(minutes, seconds);
 
-    return"";
+        //Two step approach to get hours
+        //int minutes = seconds / 60;
+        //int hours = minutes / 60;
+        //System.out.println("hours ="+hours);
 
-    public static string getDurationString(int minutes, int seconds){
-        return "";
+       // int remainingMinutes = minutes % 60;
+        //System.out.println("minutes ="+minutes);
+        //System.out.println("remainingMinutes ="+remainingMinutes);
+
+        //int remainingMinutes = seconds % 60;
+        //System.out.println("remainingSeconds ="+remainingSeconds);
+
+        //One step approach to get hours
+        //    int hours1 = minutes + seconds / 3600;
+        //    System.out.println("hours1 = " + hours1);
+
+       // return hours + "h" + remainingMinutes +"m" + remainingSeconds + "s";
+
     }
 
+    public static String getDurationString(int minutes, int seconds){
 
-    System.out.println("m")
+        int hours = minutes / 60;
 
-
+        int remainingMinutes = minutes % 60;
+        int remainingSeconds = seconds % 60;
+        return hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
+    }
 }
 
