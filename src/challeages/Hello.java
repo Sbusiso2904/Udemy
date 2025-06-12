4fdcad5165538b2dafaf7e1f7751aa78c721ac43
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-12 18:48:17 +0200

    Thanduyise

--- src/challeages/Hello.java
+++ src/challeages/Hello.java
@@ -8,7 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("challeages.Hello, Akha!");
 
-
         boolean isAlien = false;
         if (isAlien == false) {
             System.out.println("It is not an alien");
