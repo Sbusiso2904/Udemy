3e3bc525f683925b20ae8a8f8d87807143586223
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-17 11:40:52 +0200

    Thanduyise

---
+++ src/challeages/Switch.java
@@ -0,0 +1,26 @@
+package challeages;
+
+public class Switch {
+//    int value = 3;
+//    if (value == 1){
+//        System.out.println("Value was 1");
+//    } else if (value == 2){
+//        System.out.println("Value was 2");
+//    } else {
+//        System.out.println("Was not 1 or 2");
+//    }
+    int switchValue = 1;
+
+    switch (switchValue) {
+        case 1:
+            System.out.println("Value was 1");
+            break;
+        case 2:
+            System.out.println("Value was 2");
+            break;
+        default:
+            System.out.println("Was not 1 or 2");
+            break;
+    }
+    // More code
+}
