a69616ebc1bde9fee89b07bdcd31336f795a1bc1
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-17 10:35:10 +0200

    Thanduyise

--- src/challeages/getDurationString.java
+++ src/challeages/getDurationString.java
@@ -1,33 +1,38 @@
 package challeages;
+public class  Main{
+    public static void main  (String[] args) {
 
-public static void main  (String[] args){
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
+    }
+    public static String getDurationString(int seconds){
 
-        System.out.println(getDurationString(3945));
-        System.out.println(getDurationString(65, 45));
-
-    public class String getDurationString(int seconds) {
+        if (seconds  < 0){
+            return "Invalid data for seconds(" + seconds + "). moust be positive integer value";
+        }
 
         int minutes = seconds / 60;
         return getDurationString(minutes, seconds);
 
-        //Two step approach to get hours
-        //int minutes = seconds / 60;
-        //int hours = minutes / 60;
-        //System.out.println("hours ="+hours);
-
-       // int remainingMinutes = minutes % 60;
-        //System.out.println("minutes ="+minutes);
-        //System.out.println("remainingMinutes ="+remainingMinutes);
-
-        //int remainingMinutes = seconds % 60;
-
-        //System.out.println("remainingSeconds ="+remainingSeconds);
-
-        //One step approach to get hours
-        //    int hours1 = minutes + seconds / 3600;
-        //    System.out.println("hours1 = " + hours1);
-
-       // return hours + "h" + remainingMinutes +"m" + remainingSeconds + "s";
+//        //Two step approach to get hours
+//        int minutes = seconds / 60;
+//        int hours = minutes / 60;
+//        //System.out.println("hours ="+hours);
+//
+//        int remainingMinutes = minutes % 60;
+//        System.out.println("minutes ="+minutes);
+//     System.out.println("remainingMinutes ="+remainingMinutes);
+//
+//        int remainingMinutes = seconds % 60;
+//
+//        System.out.println("remainingSeconds ="+remainingSeconds);
+//
+//        //One step approach to get hours
+//            int hours1 = minutes + seconds / 3600;
+//            System.out.println("hours1 = " + hours1);
+//
+//        return hours + "h" + remainingMinutes +"m" + remainingSeconds + "s";
     }
 
     public static String getDurationString(int minutes, int seconds){
@@ -36,6 +41,7 @@ public static void main  (String[] args){
 
         int remainingMinutes = minutes % 60;
         int remainingSeconds = seconds % 60;
+
         return hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
     }
 }
