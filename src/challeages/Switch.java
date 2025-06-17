bc8962bac43d28ea54fd0bc0aee5352bf1e04e1a
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-17 11:50:31 +0200

    Thanduyise

--- src/challeages/Switch.java
+++ src/challeages/Switch.java
@@ -18,6 +18,11 @@ public class Switch {
         case 2:
             System.out.println("Value was 2");
             break;
+        case 3: case 4: case 5:
+            System.out.println("Was a 3, a 4, or a ");
+            System.out.println("Actually it was a " + switchValue);
+            break;
+
         default:
             System.out.println("Was not 1 or 2");
             break;
