0559caece044550d37bbe48df85d41cb91c641fc
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-19 11:49:11 +0200

    flop

--- src/Coding_Exercises/NumberOfDaysInMonth.java
+++ src/Coding_Exercises/NumberOfDaysInMonth.java
@@ -1,4 +1,14 @@
 package Coding_Exercises;
 
 public class NumberOfDaysInMonth {
+
+    public static String IsLeapYear(int year) {
+
+        if (year <= 1 || year <= 9999){
+            return true;
+        } else (!== year){
+            return false;
+        }
+
+    }
 }
