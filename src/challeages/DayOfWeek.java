92348fa3d3558041b82260f5f6dca324983065cb
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-19 09:58:27 +0200

    Always

--- src/challeages/DayOfWeek.java
+++ src/challeages/DayOfWeek.java
@@ -3,6 +3,14 @@ package challeages;
 public class DayOfWeek {
 
     public static void main (String[] args) {
+        printWeekDay(0);
+        printWeekDay(1);
+        printWeekDay(2);
+        printWeekDay(3);
+        printWeekDay(4);
+        printWeekDay(5);
+        printWeekDay(6);
+        printWeekDay(7);
     }
 
     public static void printDayOfWeek(int day){
@@ -22,5 +30,27 @@ public class DayOfWeek {
             default -> "Invalid DAy";
         };
             System.out.println(day + "stands for" + dayOfWeek);
+    }
+
+    public static void printWeekDay(int day) {
+        String dayOfWeek = "Invalid Day";
+        if (day == 0){
+            dayOfWeek = "Sunday";
+        } else if (day == 1) {
+            dayOfWeek = "Monday";
+        } else if (day ==  2) {
+            dayOfWeek = "Tuesday";
+        } else if (day == 3) {
+            dayOfWeek = "Wednesday";
+        } else if (day == 4) {
+            dayOfWeek = "Thrusday";
+        } else if (day == 5) {
+            dayOfWeek = "Friday";
+        } else if (day == 6) {
+            dayOfWeek = "Saturday";
+
+        }
+        System.out.println(day + "stands for " + dayOfWeek);
+
     }
 }
