4caa8d404aa4987c70880a93d6266c71a4d3faff
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-20 09:43:05 +0200

    Always

--- src/Coding_Exercises/NumberOfDaysInMonth.java
+++ src/Coding_Exercises/NumberOfDaysInMonth.java
@@ -2,31 +2,29 @@ package Coding_Exercises;
 
 public class NumberOfDaysInMonth {
 
-    public static int getDay(int year) {
-        year = (1 -9999);
+    public static int getDaysInMonth(int year, int month) {
+//        year = (1 -9999);
 
-        if (year <= 1 || year <= 9999){
+        if (year < 1 || year < 9999){
             return -1;
-//        } else (!== year){
+       }
+//        else (!== year){
 //            return false;
 //        }
 
-        int month = year;
-        switch (month){
-            case 0 -> "January";
-            case 1 -> "Febrauy";
-            case 2 -> "March";
-            case 3 -> "April";
-            case 4 -> "May";
-            case 5 -> "June";
-            case 6 -> "July";
-            case 7 -> "August";
-            case 8 -> "September";
-            case 9 -> "October";
-            case 10 -> "Novermber";
-            case 11 -> "December";
-            default -> "Invalid month"
+//        int month = year;
+        return switch (month){
+            case 1, 3, 5, 7, 8, 10, 12 -> 31; // for any of the following months, returns 31
+            case 2 -> isleapYear(year) ? 29 : 28; // if it's Feb determine if it's a leap year first, then return either 28 or 29.
+
+            case  4, 6, 9
+
         }
+
+    }
+
+    public static boolean isleapYear(int year) {
+
 
     }
 
