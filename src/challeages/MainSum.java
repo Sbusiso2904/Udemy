8a3f2d9f26f367c517f15bc343a470f1893014f2
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-24 11:31:29 +0200

    test

---
+++ src/challeages/MainSum.java
@@ -0,0 +1,24 @@
+package challeages;
+
+public class MainSum {
+
+    public static void main(String[] args) {
+        int countofMatches = 0;
+        int sumofMatches = 0;
+
+        for (int loopMumber = 1; loopMumber <= 1000; loopMumber++){
+            if ((loopMumber % 3 == 0) && (loopMumber % 5 ==0)){
+                countofMatches ++;
+                sumofMatches += loopMumber;
+                System.out.println("Found a match = " + loopMumber);
+            }
+            if (countofMatches == 5){
+                break;
+            }
+        }
+
+        System.out.println("Sum =" + sumofMatches);
+    }
+
+
+}
