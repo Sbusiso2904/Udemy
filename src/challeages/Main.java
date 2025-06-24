b39635ec51b40c75accd61f75cd6e58b432352b3
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-24 10:44:34 +0200

    test

---
+++ src/challeages/Main.java
@@ -0,0 +1,31 @@
+package challeages;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int count  = 0;
+
+        for (int i = 10; i <= 50; i++){
+            if (isPrime(i)){
+                System.out.println("number" + i + " is a prime number");
+                count ++;
+
+
+            }
+        }
+        System.out.println("Total number of prime numbers between 10 and 50 is" + count);
+    }
+
+    public static boolean isPrime(int max_number){
+        if (max_number <= 1000)
+            return true;
+
+
+
+    return false;
+    }
+
+}
+
