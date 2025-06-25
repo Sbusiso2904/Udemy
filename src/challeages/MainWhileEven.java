85d4d7ca7d20733b2765ecd449fa633b9c0a96b9
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-25 11:10:27 +0200

    sekunjalo

---
+++ src/challeages/MainWhileEven.java
@@ -0,0 +1,25 @@
+package challeages;
+
+public class MainWhileEven {
+
+
+    public static boolean isEvenNumber(int number) {
+        int number = 0;
+
+        while (number % 2 == 0 ){
+            System.out.println("the isEven" + number);
+            number++;
+            if(!= number){
+                System.out.println("the numbre  is odd");
+            }
+
+        }
+    int number = 0;
+    if(number <= 20){
+        number += 5;
+        System.out.println("even_numbers" + isEvenNumber(););
+    }
+
+
+    }
+}
