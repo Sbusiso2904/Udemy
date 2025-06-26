7c54a4c472899e13594b510f622830049b1ddf74
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-26 09:17:09 +0200

    Thanduyise

---
+++ src/challeages/sumDigits.java
@@ -0,0 +1,19 @@
+package challeages;
+
+public class sumDigits {
+    public static void main(String[] args) {
+        System.out.println("The sum of the digits in number is 1234"  + sumDigits(1234));
+        System.out.println("The sum of the digits in number is 1234"  + sumDigits(-125));
+        System.out.println("The sum of the digits in number is 1234"  + sumDigits(4));
+        System.out.println("The sum of the digits in number is 1234"  + sumDigits(321223));
+
+    }
+
+    public static int sumDigits(int number) {
+        int sum = 0;
+
+        return sum;
+
+
+    }
+}
