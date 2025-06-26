27c3fe9c13b0abd477217d4f372b8f103a86f789
nkmbathjhbo24 <nkmbathjhb024@student.wethinkcode.co.za>
2025-06-26 09:22:08 +0200

    Thanduyise

--- src/challeages/sumDigits.java
+++ src/challeages/sumDigits.java
@@ -10,7 +10,16 @@ public class sumDigits {
     }
 
     public static int sumDigits(int number) {
+        if (number < 0){
+            return -1;
+        }
         int sum = 0;
+
+        while (number > 9){
+            sum += (number % 10);
+            number = number / 10;
+        }
+        sum += number;
 
         return sum;
 
