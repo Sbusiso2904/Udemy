1b7bddbeec7897463ea8bac1cf99bfc7faee217c
nkmbajhb024 <nkmbathjhb024@student.wethinkcode.co.za>
2025-07-11 23:59:35 +0200

    Thanduyise

--- src/challeages/getDurationString.java
+++ src/challeages/getDurationString.java
@@ -12,7 +12,6 @@ public class getDurationString {
     }
     public static String getDurationString(int seconds){
 
-
         if (seconds  < 0){
             return "Invalid data for seconds("
                     + seconds + "). must be positive integer value";
