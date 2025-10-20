e1b60311ce3a1f8b69902e32fb05f29da867a78d
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-10-20 22:47:35 +0200

    working on inheritance

--- src/inheritance_execise/Main.java
+++ src/inheritance_execise/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Employee sbu = new Employee("Sbu", "29/04/2000", 
                 29292, "20/01/2026");
 
+
         System.out.println(sbu);
         System.out.println("Age =" + sbu.getAge());
         System.out.println("Pay =" + sbu.collectPay());
