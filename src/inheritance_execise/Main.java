57679fad1fd7345764ba38d243aedbacf85f60f9
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-10-21 00:03:31 +0200

    working on inheritance

--- src/inheritance_execise/Main.java
+++ src/inheritance_execise/Main.java
@@ -4,12 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
         Employee sbu = new Employee("Sbu", "29/04/2000", 
-                29292, "20/01/2026");
+                "20/01/2026");
 
 
         System.out.println(sbu);
         System.out.println("Age =" + sbu.getAge());
         System.out.println("Pay =" + sbu.collectPay());
+
+        Employee nkosi = new Employee("nkosi", "11/11/2001",
+                "03/03/2025", 70000);
+
+        System.out.println(nkosi);
+
+        System.out.println("Pay = $" + nkosi.collectPay());
+
+        nkosi.retire();
+        System.out.println("nkosi Pension check = $" + nkosi.collectPay());
 
     }
 }
--- src/inheritance_execise/SalariedEmployee.java
+++ src/inheritance_execise/SalariedEmployee.java
@@ -1,10 +1,28 @@
 package inheritance_execise;
 
-public class SalariedEmployee {
-    public double annualSalary;
-    public boolean isRetired = false;
+public class SalariedEmployee extends Employee {
 
-    public boolean retire(){
-        return isRetired;
+    double annualSalary;
+    boolean isRetired;
+
+    public SalariedEmployee(String name, String birthDate, String hireDate, double annualSalary) {
+        super(name, birthDate, hireDate);
+        this.annualSalary = annualSalary;
     }
+
+    @Override
+    public  double collectPay() {
+       double paycheck = annualSalary / 26;
+       double adjustedPay = (isRetired) ? 0.9 * paycheck : paycheck;
+
+       return (int) adjustedPay;
+    }
+
+    public void retire(){
+        terminate("12/12/2026");
+        isRetired = true;
+
+    }
+
+
 }
